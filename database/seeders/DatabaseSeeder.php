46a8f6e7ae374da30140768901747908395ffafa
MoisesAntonioBalderasLopez <moises.balderas@dsaix.com.mx>
2025-06-20 11:15:43 -0600

    agregar cambio

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,10 +7,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
         $this->call([
             CategorySeeder::class,
             ProductSeeder::class,
